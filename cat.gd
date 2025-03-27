dcb186bcdb0b7fad1fb9e6eee73fca95b8cfa8ef
Gal Krajnc <gal.krajnc@gmail.com>
2025-03-27 21:26:26 +0100

    edit 2

--- cat.gd
+++ cat.gd
@@ -10,12 +10,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get both horizontal and vertical input
-	var direction_x := Input.get_axis("ui_left", "ui_right")
-	var direction_y := Input.get_axis("ui_up", "ui_down")
+	var direction_x := Input.get_axis("move_left", "move_right")
+	var direction_y := Input.get_axis("move_up", "move_down")
 	
 	# Handle horizontal movement
 	if direction_x:
