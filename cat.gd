8538886487344b729aa9f78cf394b3991e494b6c
Gal Krajnc <gal.krajnc@gmail.com>
2025-03-29 16:01:46 +0100

    Big update

--- cat.gd
+++ cat.gd
@@ -6,17 +6,18 @@ extends CharacterBody2D
 
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -600.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") # Get the default gravity value from project settings
 
 @onready var animated_sprite_2d = $AnimatedSprite2D # Reference to the AnimatedSprite2D node
 
 # Add the gravity.
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():	# Check if the jump action is pressed and the character is on the floor
 		velocity.y = JUMP_VELOCITY
 
 	# Get both horizontal and vertical input
