6cbfd40f04c413cfb8eb8e6071078a2cef427a41
Gal Krajnc <gal.krajnc@gmail.com>
2025-04-30 17:02:07 +0200

    :)

--- sprite/Free Street Animal Pixel Art/4 Cat 2/buddy.gd
+++ sprite/Free Street Animal Pixel Art/4 Cat 2/buddy.gd
@@ -1,11 +1,11 @@
-extends CharacterBody2D
-# Cat character script for a 2D game in Godot 
-@onready var cat_speed = $Player
+extends Node2D
+@onready var cat = $"../Player"
 
-func _ready() -> void:
+func _ready():
 	$AnimatedSprite2D.flip_h = true
 	$AnimatedSprite2D.play("idle_buddy")
-func _on_area_2d_body_entered(_body) -> void:
-	print("Player has entered the area")
-	var cat_speed = 10.0
+	
+func _on_area_2d_body_entered(body: Node2D):
+	cat.SPEED = 2000
+	print(cat.SPEED)
 	pass # Replace with function body.
